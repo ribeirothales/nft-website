3ce19596832df3604dfc1ed314356efe3e13b511
Thales Ribeiro <thalesffc@gmail.com>
2024-09-26 01:07:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 
 
 var swiper = new Swiper('.mySwiper' , {
-    navigation:{
-        nextE1: ".swiper-button-next",
-        prevE1: ".swiper-button-prev",
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
+
     loop:true,
     autoplay:true,
 });
