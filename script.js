b3511f5c0ec0284b704eba8b5f5747cb22956a8c
Thales Ribeiro <thalesffc@gmail.com>
2024-09-25 23:27:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+var swiper = new Swiper('.mySwiper' , {
+    navigation:{
+        nextE1: ".swiper-button-next",
+        prevE1: ".swiper-button-prev",
+    },
+    loop:true,
+    autoplay:true,
+});
